d2fa2ea75995f66f1d461ebc802a5fbbb80386d0
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2018-03-22 16:12:28 +0000

    Reduce AnalyticsUpdateService call to one line

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,7 @@ class UsersController < ApplicationController
     if @user.save
       NotifyMailer.api_key_confirmation(@user).deliver_later
       NotifyMailer.new_api_key_request(@user).deliver_later
-      analytics_update_service = AnalyticsUpdateService.new
-      analytics_update_service.add_new_service(@user[:api_key], @user[:department], @user[:service])
+      AnalyticsUpdateService.new.add_new_service(@user[:api_key], @user[:department], @user[:service])
       render json: @user, status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
