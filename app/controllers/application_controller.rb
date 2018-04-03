6c28292e57bdd7bc12c13e0b41f7aed0fea85c31
Dan Mitchell <dan@digitalreflow.co.uk>
2018-04-03 12:49:25 +0100

    Add basic http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::API
   include ActionController::Serialization
+
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+  http_basic_authenticate_with name: Rails.application.secrets.http_auth_username, password: Rails.application.secrets.http_auth_password
 end
