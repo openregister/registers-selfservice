3fb61204ba8823b1517ab8e899562388c5a0a7d7
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-04-25 15:49:33 +0100

    lint

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,6 @@ class User < ApplicationRecord
       register_data = registers_client.get_register('government-organisation', 'beta')
       record = register_data.get_records.find { |r| department == r.entry.key }
       record.item.value['name']
-    else
-      nil
     end
   end
 
