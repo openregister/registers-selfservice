8c779b20091cfd6f726d5fcc6f130c2ea8fc98a4
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-05-08 16:02:00 +0100

    use curie to resolve department name

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,10 @@ class User < ApplicationRecord
 
   def department_name
     if department.present?
+      curie = department.split(':')
       registers_client = RegistersClient::RegisterClientManager.new
-      register_data = registers_client.get_register('government-organisation', 'beta')
-      record = register_data.get_records.find { |r| department == r.entry.key }
+      register_data = registers_client.get_register(curie[0], 'beta')
+      record = register_data.get_records.find { |r| curie[1] == r.entry.key }
       record.item.value['name']
     end
   end
