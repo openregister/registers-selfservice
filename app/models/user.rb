95bfeac789b430296daee5f0a3fd711ee7c2108c
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-06-01 15:50:45 +0100

    add validation condition

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ require 'register_client_manager'
 class User < ApplicationRecord
   before_save :set_api_key
   nilify_blanks
-  validates :contactable, inclusion: { in: [true, false] }
+  validates :contactable, inclusion: { in: [true, false] }, if: -> { is_government == true }
   validates :email, presence: true, if: -> { is_government == true }
   include ModelHelpers
 
