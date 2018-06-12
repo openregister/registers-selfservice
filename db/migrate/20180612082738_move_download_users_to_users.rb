863b8da88619f6a67a23d66023fcd6809ab799b2
Dan Mitchell <dan@digitalreflow.co.uk>
2018-06-12 17:13:25 +0100

    Update migration

--- db/migrate/20180612082738_move_download_users_to_users.rb
+++ db/migrate/20180612082738_move_download_users_to_users.rb
@@ -1,22 +1,15 @@
 class MoveDownloadUsersToUsers < ActiveRecord::Migration[5.1]
   def change
-    DownloadUsers.find_each do |download_user|
-      download_user.users.create(
+    DownloadUser.find_each do |download_user|
+      User.create(
         email: download_user.email,
         register: download_user.register,
         non_gov_use_category: download_user.non_gov_use_category,
         department: download_user.department,
-        is_government: download_user.is_government
+        is_government: download_user.is_government,
+        api_key: nil,
+        contactable: false
       )
     end
-
-    # execute <<-SQL_END
-    #   INSERT INTO users (email, register, department, is_government, non_gov_use_category)
-
-
-    #   SELECT 'DownloadUser', email, register, department, is_government, non_gov_use_category
-
-    #   FROM workshops;
-    # SQL_END
   end
 end
