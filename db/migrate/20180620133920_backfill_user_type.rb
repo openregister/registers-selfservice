b7c00a98a2a764207be5734075ad52551a7e65a5
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-06-21 09:35:30 +0100

    use ternary operator

--- db/migrate/20180620133920_backfill_user_type.rb
+++ db/migrate/20180620133920_backfill_user_type.rb
@@ -1,11 +1,7 @@
 class BackfillUserType < ActiveRecord::Migration[5.1]
   def change
     User.where(user_type: nil).find_each do |u|
-      if u.api_key.present?
-        u.update(user_type: :api)
-      else
-        u.update(user_type: :download)
-      end
+      u.api_key.present? ? u.update(user_type: :api) : u.update(user_type: :download)
     end
   end
 end
