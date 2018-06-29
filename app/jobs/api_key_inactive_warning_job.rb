c96c2021539419d4489899550fe4fc917e0396da
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-06-29 12:14:31 +0100

    use ruby generated time periods

--- app/jobs/api_key_inactive_warning_job.rb
+++ app/jobs/api_key_inactive_warning_job.rb
@@ -4,11 +4,11 @@ class ApiKeyInactiveWarningJob < ApplicationJob
   queue_as :default
 
   def perform
-    activity_days = 335
+    activity_period = 11.months.ago
     active_api_keys = GoogleAnalyticsQueries.get_active_api_keys_from_ga(activity_days)
     api_keys_older_than_activity_period = User.where(user_type: 'api')
                                                 .where.not(email: nil)
-                                                .where("created_at <= :date", date: activity_days.days.ago)
+                                                .where("created_at <= :date", date: activity_period)
                                                 .pluck(:api_key)
     inactive_api_keys = api_keys_older_than_activity_period - active_api_keys
     inactive_api_users = User.where(api_key: inactive_api_keys)
--- app/jobs/google_analytics_queries.rb
+++ app/jobs/google_analytics_queries.rb
@@ -6,7 +6,7 @@ module GoogleAnalyticsQueries
 
 module_function
 
-  def get_active_api_keys_from_ga(days_ago)
+  def get_active_api_keys_from_ga(start_date)
     authorization = ServiceAccountCredentials.make_creds(
       json_key_io: StringIO.new(Base64.decode64(Rails.application.secrets.google_api_key)),
       scope: [Google::Apis::AnalyticsreportingV4::AUTH_ANALYTICS_READONLY]
@@ -18,7 +18,7 @@ module_function
       view_id: Rails.application.secrets.google_analytics_view_id,
       dimensions: [Dimension.new(name: 'ga:dimension2')],
       metrics: [Metric.new(expression: 'ga:pageviews')],
-      date_ranges: [DateRange.new(start_date: "#{days_ago}DaysAgo", end_date: 'today')]
+      date_ranges: [DateRange.new(start_date: start_date.strftime('%Y-%m-%d'), end_date: 'today')]
       )
     response = service.batch_get_reports(GetReportsRequest.new(report_requests: [rr]))
     response.reports.first.data.rows.map { |r| r.dimensions.first }
